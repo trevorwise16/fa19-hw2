f0af7b7545bc8bfe074e71cd5522aa7b0f5a0427
Frederick Kim <freddy.kim8@gmail.com>
2019-09-24 18:11:14 -0700

    Fix README

--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,13 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
 end
 
 def blockin_time(a)
